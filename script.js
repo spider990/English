97bb6cab420fbd72d3c8c04486275ec001eea643
spider990 <85675202+spider990@users.noreply.github.com>
2022-03-07 18:17:23 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+const button = document.querySelector('.GG');
+
+const mouseHoverAnimation = function() {
+    anime({
+        targets: '.GG',
+        width: '50%',
+        scale: {
+            delay: 500,
+            value: 1.5,
+        },
+        duration: 1500
+    })
+}
+
+const mouseOutAnimation = function() {
+    anime({
+        targets: '.GG',
+        width: '80px',
+        scale: {
+            delay: 800,
+            value: 1,
+        },
+        duration: 1500
+    })
+}
+
+button.addEventListener('mouseover', mouseHoverAnimation)
+button.addEventListener('mouseout', mouseOutAnimation)
+
+
+
+anime({
+    targets: '.box .block',
+    trnsalteY: 270,
+    scale:[
+        {value: .1, easing:'easeOutSine', duration: 800},
+        {value: 1, easing: 'easeInOutQuad', duration: 800},
+    ],
+    delay: anime.stagger(200, {grid: [10, 4], from:'center'}),
+    loop: true
+});
